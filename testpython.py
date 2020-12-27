1c87a8b66aacbf21f7a8116beab49d0e5b649509
Yavuz Ahmet Yoldas <yavuz.ahmet.yoldas@huawei.com>
2020-12-27 18:40:52 +0300

    python test first commit

---
+++ testpython.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+import time 
+browser = webdriver.Chrome(executable_path=r'C:\chromedriver_win32\chromedriver.exe')
+browser.get("https://developer.huawei.com/consumer/en/")
+
+signIn = browser.find_element_by_xpath("//*[@id='portal_login']");
+time.sleep(3)
+
+signIn.click()
+time.sleep(5)
+
+
+browser.close()
