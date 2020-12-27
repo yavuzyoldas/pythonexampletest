3e0164a588e872db962f9714da29b77a4c9010ab
Yavuz Ahmet Yoldas <yavuz.ahmet.yoldas@huawei.com>
2020-12-27 21:41:00 +0300

    syntax error is fixed.

--- testpython.py
+++ testpython.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 import time 
-browser = webdriver.Chrome(executable_path=r'C:\chromedriver_win32\chromedriver.exe')
+browser = webdriver.Chrome('C:\chromedriver_win32\chromedriver.exe')
 browser.get("https://developer.huawei.com/consumer/en/")
 
 signIn = browser.find_element_by_xpath("//*[@id='portal_login']");
